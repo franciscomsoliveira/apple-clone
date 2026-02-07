882e7c0973346574af8c6b878e6772e621a9c242
Leicam84_DEV <leicam84@gmail.com>
2026-02-07 00:36:02 -0300

    heroBase

--- src/pages/Home/HeroContent.tsx
+++ src/pages/Home/HeroContent.tsx
@@ -1,5 +1,8 @@
 export const HeroContent = () => (
   <>
+    {/* Note que o caminho começa na raiz '/' que aponta para 'public' */}
+    <img src="/assets/980x551.jpg" alt="iPhone 15 Pro em titânio" />
+
     <h1>iPhone 15 Pro</h1>
     <p>Titânio. Muito resistente. Muito leve.</p>
   </>
